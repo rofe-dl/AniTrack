0e531aad683504180cf82d6dc84c0d050197addd
rofe-dl <rafidul258@gmail.com>
2021-07-21 19:55:29 +0600

    added shadow under nav, changed var names

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const route = express.Router();
 
-const services = require('../services/render');
+const render = require('../services/render');
 
 //  function to run when root url is called
-route.get('/', services.homeRoutes);
+route.get('/', render.homeRoutes);
 
 // line used to define what gets returned when require() is called on this file
 module.exports = route;
