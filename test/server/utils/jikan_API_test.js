847277129be8392ea14bd8a53511429a59e1276d
rofe-dl <rafidul258@gmail.com>
2021-09-19 00:42:24 +0600

    wrote a test for jikan_API

---
+++ test/server/utils/jikan_API_test.js
@@ -0,0 +1,14 @@
+const assert = require('chai').assert;
+const jikan_API = require('../../../server/utils/jikan_API');
+
+describe('Jikan API', () => {
+    it('getFrontPageAnime should return arrays of 5 anime objects each', async () => {
+        const result = await jikan_API.getFrontPageAnime();
+        assert.equal(result.comedy.length, 5);
+    });
+
+    it('getAnimeInfo should return details of Cowboy Bebop', async() => {
+        const result = await jikan_API.getAnimeInfo(1);
+        assert.equal(result.title, 'Cowboy Bebop');
+    })
+});
